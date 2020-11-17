d43e7a0ddc4d1e0510936c171042f8cb5b96b718
miltonrubro@gmail.com <miltonrubro@gmail.com>
2020-11-16 23:07:47 -0300

    Criando metodo para buscar livro pelo id

--- src/main/java/br/com/devmos/apilivraria/controllers/LivrosController.java
+++ src/main/java/br/com/devmos/apilivraria/controllers/LivrosController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,12 @@ public class LivrosController {
 	@GetMapping("api/livros")
 	public ResponseEntity<List<LivroDTO>> getLivros(){
 		return ResponseEntity.ok(livroService.listarLivros());
+	}
+	
+	@GetMapping("api/livros/{idLivro}")
+	public ResponseEntity<Livro> getLivro(@PathVariable("idLivro") Long id){
+		Livro livro = livroService.buscarLivro(id);
+		return ResponseEntity.ok(livro);
 	}
 
 }
--- src/main/java/br/com/devmos/apilivraria/services/LivroService.java
+++ src/main/java/br/com/devmos/apilivraria/services/LivroService.java
@@ -1,6 +1,7 @@
 package br.com.devmos.apilivraria.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -9,6 +10,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.devmos.apilivraria.errors.ResourceNotFoundException;
 import br.com.devmos.apilivraria.models.Livro;
 import br.com.devmos.apilivraria.models.LivroDTO;
 import br.com.devmos.apilivraria.repositories.LivroRepository;
@@ -31,6 +33,15 @@ public class LivroService {
 	public List<LivroDTO> listarLivros() {
 		List<Livro> livros = livroRepository.findAll();
 		return LivroDTO.converter(livros);
+	}
+
+	public Livro buscarLivro(Long id) {
+		Optional<Livro> possivelLivro = livroRepository.findById(id);
+		
+		if(!possivelLivro.isPresent()) {
+			throw new ResourceNotFoundException();
+		}
+		return possivelLivro.get();
 	}
 
 }
