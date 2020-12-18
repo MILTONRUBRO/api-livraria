d9d02145aa2c6f073a945c33329a29f466d7d9a4
miltonrubro@gmail.com <miltonrubro@gmail.com>
2020-12-17 22:26:53 -0300

    Adicionando validacao na compra

--- src/main/java/br/com/devmos/apilivraria/models/CompraRequest.java
+++ src/main/java/br/com/devmos/apilivraria/models/CompraRequest.java
@@ -1,16 +1,53 @@
 package br.com.devmos.apilivraria.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class CompraRequest {
+	
+	@Email
+	@NotBlank
 	private String email;
+	@NotBlank
 	private String nome;
+	@NotBlank
 	private String sobrenome;
+	@NotBlank
 	private String documento;
+	@NotBlank
 	private String endereco;
+	@NotBlank
 	private String complemento;
+	@NotBlank
 	private String cidade;
+	@NotNull
 	private Long idPais;
+	@NotNull
 	private Long idEstado;
+	@NotBlank
 	private String telefone;
+	@NotBlank
 	private String cep;
+	
+	public CompraRequest(@Email @NotBlank String email, @NotBlank String nome, @NotBlank String sobrenome,
+			@NotBlank String documento, @NotBlank String endereco, @NotBlank String complemento,
+			@NotBlank String cidade, @NotNull Long idPais, @NotNull Long idEstado, @NotBlank String telefone,
+			@NotBlank String cep) {
+		super();
+		this.email = email;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.documento = documento;
+		this.endereco = endereco;
+		this.complemento = complemento;
+		this.cidade = cidade;
+		this.idPais = idPais;
+		this.idEstado = idEstado;
+		this.telefone = telefone;
+		this.cep = cep;
+	}
+	
+	
 
 }
